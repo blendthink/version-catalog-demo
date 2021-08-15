09bf8147fabbd8c8c9ed5ddee74f2411ccd1ef80
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2021-08-15 23:41:18 +0900

    update compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val composeVersion: String by extra { "1.0.0" }
+    val composeVersion: String by extra { "1.0.1" }
     repositories {
         google()
         mavenCentral()
